710f63e9f86140f1f363099ed3a2f3729af10bae
Ikuomola Stephen <cstephenid@yahoo.com>
2022-12-08 19:55:35 +0100

    modify name of javascript folder

--- src/js/main.js
+++ src/js/main.js
@@ -10,16 +10,9 @@ const observerOptions = {
 
 
 const handleInterSection = function(entries, observer) {
-  // The handleInterSection receives an IntersectionObserverEntry and the observer
-
-  // The IntersectionObserverEntry is a list of entries of for each section element which has experienced an intersection change since it was last checked. 
   entries.forEach(function(entry) {
     if(entry.isIntersecting) {
-      
-      // Remove the class that hides the sections
       entry.target.classList.remove('section__hidden');
-
-      // Then stop watch the intersections target element/section might or might not have with the root. 
       observer.unobserve(entry.target); 
     } 
   }); 
@@ -28,11 +21,8 @@ const handleInterSection = function(entries, observer) {
 
 
 let observer = new IntersectionObserver(handleInterSection, observerOptions); 
-
-// Since the sectionElements is  NodeList I can loop over it and then add the "section__hidden" class to each element. 
+ 
 sectionElements.forEach(function(section) {
   section.classList.add('section__hidden');
-  
-  // I need to give the observer an element to watch which is the target element (section)
   observer.observe(section); 
 }); 
